0b78270fad4c77ed0803a160b848b12b44eeaa96
Karl Koscher <github@degdeg.com>
2020-12-26 03:52:18 +0000

    Add codeplug flashing PoC

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import { DFU } from './dfu';
-import { MD380, MD380DFU, TYTDFU } from './tyt';
+import { MD380, MD380DFU, MDUV380DFU, TYTDFU } from './tyt';
 
 function requestRadioAccess() {
-    MD380DFU.requestDevice()
+    MDUV380DFU.requestDevice([
+            { 'vendorId': 0x0483, 'productId': 0xdf11 },
+            { 'vendorId': 0x28e9, 'productId': 0x018a, }])
         .then((device) => TYTDFU.fromUsbDevice(device))
         .then((dfu) => window.dfu = dfu);
     window.md380 = new MD380();
+}
+
+function downloadCodePlug(dfu) {
+    fetch('https://webcps.ky0lo.com/xxxxxxx.bin')
+    .then(response => response.arrayBuffer())
+    .then(data => programCodePlug(dfu, data));
+}
+
+async function programCodePlug(dfu, codePlug) {
+    await dfu.putCodePlug(codePlug);
+    await dfu.setTime();
 }
 
 let connectToRadioBtn = document.createElement('button');
